e6765d9b6f5d7959242036b3c17657faf06fc196
Eric Chin <ericychin94@gmail.com>
2021-09-12 03:50:57 -0700

    Added problem 1

---
+++ Arrays And Strings/IsUnique.java
@@ -0,0 +1,14 @@
+/*
+Implement an algorithm to determine if a string has all unique characters.
+What if you cannot use additional data structures?
+*/
+
+public class IsUnique {
+    public static boolean isUnique(String str) {
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
