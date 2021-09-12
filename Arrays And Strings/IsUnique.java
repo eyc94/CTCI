4b4fa51a3e1fa03e97afaaf1d8e7eea7c9c5fe22
Eric Chin <ericychin94@gmail.com>
2021-09-12 15:18:32 -0700

    Removed Hash Set for hash table

--- Arrays And Strings/IsUnique.java
+++ Arrays And Strings/IsUnique.java
@@ -5,35 +5,38 @@ Implement an algorithm to determine if a string has all unique characters.
 What if you cannot use additional data structures?
 */
 
-import java.util.*;
-
 public class IsUnique {
 
     /*
-     * This solution uses a Hash Set to determine whether our string has duplicate
+     * This solution uses a hash table to determine whether our string has unique
      * characters. We iterate through our string and determine whether we have
      * already seen the character in the string or not. If we did see the character,
      * we return false because that's a duplicate. If we have not, we NOW see it, so
-     * add this character to the Hash Set.
+     * mark our hash table spot as true.
      * 
      * Time Complexity: O(N) where N is the length of our string.
      * 
-     * Space Complexity: O(N) where N is the length of our string. If our string has
-     * no duplicate characters, the Hash Set will have the same length as our
-     * string.
+     * Space Complexity: O(1) because there is a fixed amount of characters at any
+     * time and this does not depend on the length of the string.
      */
 
     public static boolean isUnique(String str) {
-        // We create a Hash Set that keeps track of characters seen.
-        HashSet<Character> seen = new HashSet<>();
+        // If we have more characters in our string than there are available, at least
+        // one is not unique.
+        if (str.length() > 128)
+            return false;
 
-        // Iterate through our character array.
+        // We create a hash table that keeps track of characters seen.
+        // True means the character is in the set.
+        boolean[] charSet = new boolean[128];
+
+        // Iterate through our string.
         for (char c : str.toCharArray()) {
-            if (seen.contains(c)) { // If we have seen the character before.
+            if (charSet[c]) { // If our character has already been seen (is true).
                 return false; // Return false.
-            } else { // If we have not seen the character.
-                seen.add(c); // Add the character to the Hash Set.
             }
+            // This means we have not seen the character in our string.
+            charSet[c] = true; // Set that position to true because NOW we see it.
         }
 
         // Return true if we make it to the end with no duplicates found.
@@ -42,7 +45,7 @@ public class IsUnique {
 
     public static void main(String[] args) {
         String sampleOne = "alphabet";
-        String sampleTwo = "unique";
+        String sampleTwo = "extra";
         System.out.println(sampleOne + " is unique: " + isUnique(sampleOne));
         System.out.println(sampleTwo + " is unique: " + isUnique(sampleTwo));
     }
