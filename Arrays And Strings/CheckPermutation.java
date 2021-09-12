e5fdc52ccb3f29e987768e9a55c1047dc6fad53d
Eric Chin <ericychin94@gmail.com>
2021-09-12 11:41:14 -0700

    Added title and problem statement

--- Arrays And Strings/CheckPermutation.java
+++ Arrays And Strings/CheckPermutation.java
@@ -1,4 +1,13 @@
+/*
+-- Check Permutation --
+
+Given two strings, write a method to decide if one is a permutation of the other.
+*/
+
 public class CheckPermutation {
+
+    /*
+    */
 
     public static boolean checkPermutation(String str1, String str2) {
         if (str1.length() != str2.length()) {
