8d835315cb52c0e033775f68a8814aa067cd755b
Eric Chin <ericychin94@gmail.com>
2021-09-13 02:38:54 -0700

    Added ZeroMatrix template

---
+++ Arrays And Strings/ZeroMatrix.java
@@ -0,0 +1,17 @@
+/*
+-- Zero Matrix --
+
+Write an algorithm such that if an element in an MxN matrix is 0, its
+entire row and column are set to 0.
+*/
+
+public class ZeroMatrix {
+
+    public static void zeroMatrix(int[][] matrix) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
