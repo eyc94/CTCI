4c78e245bbf37d0a26e99deac44ac9e421a9b8c7
Eric Chin <ericychin94@gmail.com>
2021-09-13 15:53:52 -0700

    Added code and comments for Partition

--- Linked Lists/Partition.java
+++ Linked Lists/Partition.java
@@ -13,41 +13,76 @@ Output:     3 -> 1 -> 2 -> 10 -> 5 -> 5 -> 8
 */
 
 public class Partition {
-    public static void partition(Node node, int x) {
-        Node beforeStart = null;
-        Node beforeEnd = null;
-        Node afterStart = null;
-        Node afterEnd = null;
 
+    /*
+     * This solution is easy. We basically just create two Linked Lists. The left
+     * half contains all node values less than 'x'. The right half will contain all
+     * values greater than or equal to x.
+     * 
+     * We then merge the two lists together.
+     * 
+     * We keep a reference to the head and tails of both the left and right halves.
+     * 
+     * Time Complexity: O(N) where N is the length of the linked list.
+     * 
+     * Space Complexity: O(1) because we do not use additional space.
+     */
+
+    public static void partition(Node node, int x) {
+        // Create pointers to the start and end nodes of the left and right halves.
+        Node beforeStart = null; // Head of left half.
+        Node beforeEnd = null; // Tail of left half.
+        Node afterStart = null; // Head of right half.
+        Node afterEnd = null; // Tail of right half.
+
+        // Iterate while node is not null.
         while (node != null) {
+            // Make a reference to the next node to look at.
             Node next = node.next;
+            // Detach the current node from its list.
             node.next = null;
 
+            // If the current node's value is less than partition value x.
             if (node.data < x) {
+                // If left half is empty.
                 if (beforeStart == null) {
+                    // Left head points to the current node.
                     beforeStart = node;
+                    // Left tail points to the current node.
                     beforeEnd = beforeStart;
-                } else {
+                } else { // If left half is NOT empty.
+                    // Left tail's next pointer points to current node.
                     beforeEnd.next = node;
+                    // Left tail updates to added node.
                     beforeEnd = node;
                 }
-            } else {
+            } else { // If the current node's value is greater than or equal to partition value x.
+                // If the right half is empty.
                 if (afterStart == null) {
+                    // Add the current node to the right half as its head.
                     afterStart = node;
+                    // Update the right half tail to point to node.
                     afterEnd = afterStart;
-                } else {
+                } else { // If the right half is NOT empty.
+                    // Point the right tail's next point to the new node to add.
                     afterEnd.next = node;
+                    // Update the tail to be the new node added.
                     afterEnd = node;
                 }
             }
+            // Move the current node to the next node to process.
             node = next;
         }
 
+        // If our left half is empty (every node is greater than or equal to x).
         if (beforeStart == null) {
+            // Return the right half.
             return afterStart;
         }
 
+        // If our left half is NOT empty, connect left half tail to the right half head.
         beforeEnd.next = afterStart;
+        // Return the head of the left half, as it is the head of the whole thing.
         return beforeStart;
     }
 }
