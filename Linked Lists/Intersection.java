4cf207288856e2ee40ad26cd232d788ddef82bf6
Eric Chin <ericychin94@gmail.com>
2021-09-13 18:21:30 -0700

    Added empty files

---
+++ Linked Lists/Intersection.java
@@ -0,0 +1,12 @@
+/*
+-- Intersection --
+
+Given two (singly) linked lists, determine if the two lists intersect. Return the intersecting
+node. Note that the intersection is defined based on reference, not value. That is, if the kth
+node of the first Linked List is the exact same node (by reference) as the jth node of the
+second Linked List, then they are intersecting.
+*/
+
+public class Intersection {
+
+}
---
+++ Linked Lists/LoopDetection.java
@@ -0,0 +1,10 @@
+/*
+-- Loop Detection --
+
+Given a circular linked list, implement an algorithm that returns the node at the
+beginning of the loop.
+*/
+
+public class LoopDetection {
+
+}
